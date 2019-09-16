e34a88dc8cf1ad8a0ee109e46a826cf599b3e698
Edward Teixeira <etdj@aluno.ifnmg.edu.br>
2019-09-15 22:52:03 -0400

    Finished the EdgeLedger Website with Google Maps API and Responsiveness

--- Projects/EdgeLedger/js/main.js
+++ Projects/EdgeLedger/js/main.js
@@ -1,10 +1,10 @@
 // Initialize and add the map
 function initMap() {
   // Your location
-  const loc = { lat: 42.361145, lng: -71.057083 };
+  const loc = { lat: -16.17954, lng: -40.69906 };
   // Centered map on location
   const map = new google.maps.Map(document.querySelector(".map"), {
-    zoom: 14,
+    zoom: 15,
     center: loc
   });
   // The marker, positioned at location
@@ -18,14 +18,11 @@ window.addEventListener("scroll", function() {
     document.querySelector("#navbar").style.opacity = 1;
   }
 });
-
 // Smooth Scrolling
 $("#navbar a, .btn").on("click", function(event) {
   if (this.hash !== "") {
     event.preventDefault();
-
     const hash = this.hash;
-
     $("html, body").animate(
       {
         scrollTop: $(hash).offset().top - 100
